2f5ce5d678a3eb7a4e95697fdfc3d2029a4c90ae
V2 <me123opportunity@gmail.com>
2025-03-03 13:55:31 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Spectre.Console;
+
+var menuChoices = new string[3] {"View Books", "Add Book", "Delete Book"};
+
+var choice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>().Title("What do you want to do next?").AddChoices(menuChoices));
+
